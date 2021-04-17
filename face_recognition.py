b60cc009f31907dbdeaef67bbd2996bfb29997c7
B. Chenna Krishna Reddy <73589200+bckr2549@users.noreply.github.com>
2021-04-18 00:32:34 +0530

    Add files via upload

---
+++ face_recognition.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+haar_cascade = cv2.CascadeClassifier('haar_face.xml')
+people = ['Pranab Mukherjee','Elton John', 'Jerry Seinfield']
+#features = np.load('features.npy', allow_pickle=True)
+#labels = np.load('labels.npy',)
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('face_trained.yml')
+
+img= cv2.imread(r'C:\Users\CHENNA KRISHNA\Desktop\open cv\.vscode\faces\Pranab Mukherjee\6.png')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Person', gray)
+
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces_rect:
+    faces_roi = gray[y:y+h,x:x+w]
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f'Label = {people[label]} with a confidence of {confidence}')
+
+    cv2.putText(img, str(people[label]), (20,20), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), thickness=2)
+    cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),thickness=4)
+
+cv2.imshow('Detected Face', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
---
+++ faces_train.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import os
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+#p = []
+#for i in os.listdir(r'C:\Users\CHENNA KRISHNA\Desktop\open cv\.vscode\faces'):
+  #  p.append(i)
+#print(p)
+DIR = r'C:\Users\CHENNA KRISHNA\Desktop\open cv\.vscode\faces'
+haar_cascade = cv2.CascadeClassifier('haar_face.xml')
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            img_array = cv2.imread(img_path)
+            gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors = 4)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+print('Training done ---------------')
+features = np.array(features, dtype = 'object')
+labels = np.array(labels)
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+face_recognizer.train(features,labels)
+face_recognizer.save('face_trained.yml')
+np.save('features.npy',features)
+np.save('labels.npy', labels)
